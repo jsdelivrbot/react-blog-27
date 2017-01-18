56bbfd2fec077b64677a55216816512ef61a666c
tunca.tunc <tunca.tunc@pro-line.com.tr>
2017-01-18 17:52:51 +0300

    contd.

--- src/components/post-index.js
+++ src/components/post-index.js
@@ -1,33 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
-const renderBlog = ({id, title, categories}) => {
-  return(
-    <tr key={id} valign="bottom">
-      <td>{title}</td>
-      <td>{categories}</td>
-    </tr>
-  );
-}
+import fetchPosts from '../actions/index'
 
-export default (props) => {
-  console.log(props);
-  return(
-    <div>
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>
-              Title
-            </th>
-            <th>
-              Categories
-            </th>
-          </tr>          
-        </thead>
-        <tbody>
-          {props.posts.map(renderBlog)}
-        </tbody>
-      </table>
-    </div>
-  );
+class PostIndex extends Component{
+  componentWillMount() {
+    console.log('Call Action creator to fetch blog posts.');
+    this.props.fetchPosts();
+  }
+
+  renderBlog ({id, title, categories}) {
+    return(
+      <tr key={id} valign="bottom">
+        <td>{title}</td>
+        <td>{categories}</td>
+      </tr>
+    );
+  }
+
+  render(){
+    console.log(this.props);
+    return(
+      <div>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>
+                Title
+              </th>
+              <th>
+                Categories
+              </th>
+            </tr>          
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  bindActionCreators({fetchPosts}, dispatch);
 }
+export default connect(null, mapDispatchToProps)(PostIndex);
