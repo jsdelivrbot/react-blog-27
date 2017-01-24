75dd2308f3fdb963407c83d80876aa01010649ff
tunca.tunc <tunca.tunc@pro-line.com.tr>
2017-01-24 14:18:28 +0300

    react-dev-tools are added.

--- src/index.js
+++ src/index.js
@@ -4,16 +4,22 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router'
 import promise from 'redux-promise'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducers from './reducers';
 import routes from './routes'
+
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(promise),
+  // other store enhancers if any
+));
 
 const createStoreWithMiddleware = applyMiddleware(
   promise
 )(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory} routes={routes}/>
   </Provider>
   , document.querySelector('.container'));
